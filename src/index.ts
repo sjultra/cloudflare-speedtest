294c3c7a43c4d3f8e6655b0da4c335b5befdc28d
DickermanJosh <123334042+DickermanJosh@users.noreply.github.com>
2025-03-24 12:38:53 -0700

    Timing results printing to console. Added vite and puppeteer to launch @cloudflare/speedtest in a headless chromium instance.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello World");
+import path from 'path';
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  // page.on('console', (msg) => console.log('[BROWSER LOG]', msg.text()));
+  // page.on('pageerror', (err) => console.error('[BROWSER ERROR]', err));
+
+  // Construct a filepath to speedtest.html to run in the browser
+  const filePath = path.resolve(__dirname, 'speedtest.html');
+  const fileUrl = 'file://' + filePath;
+
+  await page.goto(fileUrl);
+
+  // Wait for results from the browser
+  await page.waitForFunction(() => {
+    return window.SpeedTestResults !== null || window.SpeedTestError !== null;
+  }, { timeout: 2 * 60_000 }); // 2 minute timeout
+
+  const { results, error } = await page.evaluate(() => {
+    return {
+      results: window.SpeedTestResults,
+      error: window.SpeedTestError
+    };
+  });
+
+  if (results !== null) {
+    console.log('SpeedTest results:', results);
+  } else {
+    console.error('SpeedTest encountered an error:', error);
+  }
+
+  await browser.close();
+})();
+

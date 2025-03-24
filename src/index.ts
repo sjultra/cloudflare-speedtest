eb871d02093928c1753df246dd7c4b66101a351b
DickermanJosh <123334042+DickermanJosh@users.noreply.github.com>
2025-03-24 13:03:57 -0700

    Run tests in an infinite loop

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,40 @@ import path from 'path';
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+    const INTERVAL = 30_000;
 
-  // page.on('console', (msg) => console.log('[BROWSER LOG]', msg.text()));
-  // page.on('pageerror', (err) => console.error('[BROWSER ERROR]', err));
+    const browser = await puppeteer.launch({ headless: true });
 
-  // Construct a filepath to speedtest.html to run in the browser
-  const filePath = path.resolve(__dirname, 'speedtest.html');
-  const fileUrl = 'file://' + filePath;
+    // Construct a filepath to speedtest.html to run in the browser
+    const filePath = path.resolve(__dirname, 'speedtest.html');
+    const fileUrl = 'file://' + filePath;
 
-  await page.goto(fileUrl);
+    while (true) {
+        const page = await browser.newPage();
+        await page.goto(fileUrl);
 
-  // Wait for results from the browser
-  await page.waitForFunction(() => {
-    return window.SpeedTestResults !== null || window.SpeedTestError !== null;
-  }, { timeout: 2 * 60_000 }); // 2 minute timeout
+        console.log('Starting new SpeedTest.\n');
 
-  const { results, error } = await page.evaluate(() => {
-    return {
-      results: window.SpeedTestResults,
-      error: window.SpeedTestError
-    };
-  });
+        // Wait for results from the browser
+        await page.waitForFunction(() => {
+            return window.SpeedTestResults !== null || window.SpeedTestError !== null;
+        }, { timeout: 2 * 60_000 }); // 2 minute timeout
 
-  if (results !== null) {
-    console.log('SpeedTest results:', results);
-  } else {
-    console.error('SpeedTest encountered an error:', error);
-  }
+        const { results, error } = await page.evaluate(() => {
+            return {
+                results: window.SpeedTestResults,
+                error: window.SpeedTestError
+            };
+        });
 
-  await browser.close();
+        if (results !== null) {
+            console.log('SpeedTest results:', results, '\n');
+        } else {
+            console.error('SpeedTest encountered an error:', error, '\n');
+        }
+
+        await page.close();
+        await new Promise((resolve) => setTimeout(resolve, INTERVAL));
+    }
 })();
 
